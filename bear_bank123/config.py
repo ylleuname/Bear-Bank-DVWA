0c1a1d91d2d6d231fa2b34171e40b8017baf500b
Emanuelly <onlyfstudy@gmail.com>
2025-02-10 15:07:17 -0300

    Update config.py

--- bear_bank123/config.py
+++ bear_bank123/config.py
@@ -3,13 +3,13 @@ import os
 DB_CONFIG = {
     'host': 'db', # Usa 'db' como host do MySQL seguro
     'user': 'root',
-    'password': 'root',
+    'password': 'admin',
     'database': 'bear_bank'
 }
 
 DB_CONFIG_VULNERABLE = {
     'host': 'db_vulnerable',
     'user': 'root',
-    'password': 'root',
+    'password': 'admin',
     'database': 'bear_bank_v'
 }
